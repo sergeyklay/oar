f299745b91f7eb6cd7794e999651debcac618864
Serghei Iakovlev <git@serghei.pl>
2025-12-04 11:27:32 +0100

    Setup test coverage for jest

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
   },
 
   clearMocks: true,
+  reporters: [
+    "default",
+    [ "jest-junit", { classNameTemplate: "{filepath}" } ]
+  ],
 }
 
 export default createJestConfig(config)
